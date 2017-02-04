71ca4bb0e02dbcb6aa8b40b285de06735aa8ce75
Nicholas Ngorok <ny2ko7@gmail.com>
2017-02-03 16:51:27 -0800

    Allow install of in house kafka 0.9.0.2

--- manifests/install.pp
+++ manifests/install.pp
@@ -1,7 +1,11 @@
 class kafka::install {
-
-  package { 'confluent-kafka':
-    ensure => $kafka::version,
+  if $kafka::version == '0.9.0.2' {
+    package { 'kafka':
+      ensure => $kafka::version,
+    }
+  } else{
+    package { 'confluent-kafka':
+      ensure => $kafka::version,
+    }
   }
-
 }
