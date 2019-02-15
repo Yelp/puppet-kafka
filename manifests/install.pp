304fb5159da735e58e30c6da70f603808bea6931
Brian Sang <baisang@yelp.com>
2019-02-14 18:58:50 -0800

    Remove references to kafka 0.10.2.0

--- manifests/install.pp
+++ manifests/install.pp
@@ -1,17 +1,8 @@
 class kafka::install {
-  if $kafka::version in ['0.10.2.2-yelp1'] {
-    package { 'confluent-kafka':
-      ensure => 'absent',
-    } ->
-    package { 'kafka':
-      ensure => $kafka::version,
-    }
-  } else{
-    package { 'kafka':
-      ensure => 'absent',
-    } ->
-    package { 'confluent-kafka':
-      ensure => $kafka::version,
-    }
+  package { 'kafka':
+    ensure => 'absent',
+  } ->
+  package { 'confluent-kafka':
+    ensure => $kafka::version,
   }
 }
